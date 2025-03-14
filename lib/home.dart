aed46e6d9566619d537ba4008593c020af4ed8e8
marcelo paiva <marcelo.paiva@crmvmg.gov.br>
2025-03-14 16:36:43 -0300

    new home blueprint

--- lib/home.dart
+++ lib/home.dart
@@ -2,12 +2,6 @@ import 'package:flutter/material.dart';
 import 'constants.dart';
 import 'components/theme_button.dart';
 import 'components/color_button.dart';
-import 'components/category_card.dart';
-import '../models/food_category.dart';
-import 'components/post_card.dart';
-import '../models/post.dart';
-import 'components/restaurant_landscape_card.dart';
-import '../models/restaurant.dart';
 
 class Home extends StatefulWidget {
   final void Function(bool useLightMode) changeTheme;
@@ -29,54 +23,50 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int tab = 0;
-  List<NavigationDestination> appBarDestinations =[
+  List<NavigationDestination> appBarDestinations = const [
     NavigationDestination(
-      icon: Icon(Icons.credit_card),
-      label: 'Category',
-      selectedIcon: Icon(Icons.credit_card),
+      icon: Icon(Icons.home_outlined),
+      label: 'Explore',
+      selectedIcon: Icon(Icons.home),
     ),
     NavigationDestination(
-      icon: Icon(Icons.credit_card),
-      label: 'Post',
-      selectedIcon: Icon(Icons.credit_card),
+      icon: Icon(Icons.list_outlined),
+      label: 'Orders',
+      selectedIcon: Icon(Icons.list),
     ),
     NavigationDestination(
-      icon: Icon(Icons.credit_card),
-      label: 'Restaurant',
-      selectedIcon: Icon(Icons.credit_card),
-    ),
+      icon: Icon(Icons.person_2_outlined),
+      label: 'Account',
+      selectedIcon: Icon(Icons.person),
+    )
   ];
   
   @override
   Widget build(BuildContext context) {
     final pages = [
-      Center(
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 300),
-          child: CategoryCard(
-            category: categories[0],
-          ),
+      const Center(
+        child: Text(
+          'Explore',
+          style: TextStyle(fontSize: 32.0),
         ),
       ),
-      Center(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: PostCard(
-            post: posts[0],
-          ),
+      const Center(
+        child: Text(
+          'Order Page',
+          style: TextStyle(fontSize: 32.0),
         ),
       ),
-      Center(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(maxWidth: 400),
-          child: RestaurantLandscapeCard(
-            restaurant: restaurants[0],
-          ),
+      const Center(
+        child: Text(
+          'Account Page',
+          style: TextStyle(fontSize: 32.0),
         ),
-      )
+      ),
     ];
+    
     return Scaffold(
       appBar: AppBar(
+        title: Text(widget.appTitle),
         actions: <Widget>[
           ThemeButton(changeThemeMode: widget.changeTheme),
           ColorButton(changeColor: widget.changeColor, colorSelected: widget.colorSelected),
@@ -84,12 +74,9 @@ class _HomeState extends State<Home> {
         elevation: 4.0,
         backgroundColor: Theme.of(context).colorScheme.surface,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: IndexedStack(
-          index: tab,
-          children: pages,
-        ),
+      body: IndexedStack(
+        index: tab,
+        children: pages,
       ),
       bottomNavigationBar: NavigationBar(
         destinations: appBarDestinations,
