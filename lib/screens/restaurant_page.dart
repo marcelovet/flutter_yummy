9f7ee97dc70705747d4e685b1e1c5f26b403f88e
marcelo paiva <marcelo_thelin@hotmail.com>
2025-03-16 09:16:33 -0300

    Custom scroll view into restaurant page with placeholders

--- lib/screens/restaurant_page.dart
+++ lib/screens/restaurant_page.dart
@@ -15,7 +15,32 @@ class _RestaurantPageState extends State<RestaurantPage> {
   // TODO: Calculate constraint width
   // TODO: add calculate column count
 
-  // TODO: build custom scroll view
+  CustomScrollView _buildCustomScrollView() {
+    return CustomScrollView(
+      slivers: [
+        // TODO: add sliver app bar
+        SliverToBoxAdapter(
+          child: Container(
+            height: 200.0,
+            color: Colors.red,
+          ),
+        ),
+        // TODO: add info section
+        SliverToBoxAdapter(
+          child: Container(
+            height: 200.0,
+            color: Colors.green,
+          ),
+        ),
+        // TODO: add menu item grid view section
+        SliverFillRemaining(
+          child: Container(
+            color: Colors.blue,
+          ),
+        )
+      ],
+    );
+  }
   // TODO: build sliver app bar
   // TODO: build info section
   // TODO: build grid item
@@ -28,11 +53,7 @@ class _RestaurantPageState extends State<RestaurantPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        // TODO: replace with custom scroll view
-        child: Text(
-          "Restaurant page",
-          style: TextStyle(fontSize: 16.0),
-        ),
+        child: _buildCustomScrollView(),
       ),
     );
   }
